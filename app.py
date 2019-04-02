76ea4dc52fd379310f6fe045e8e52034565dde20
Kelly de Vries <kdv@me.com>
2019-04-02 09:07:02 -0700

    add link to title & pass to home template to render

--- app.py
+++ app.py
@@ -13,12 +13,13 @@ headers = {'Content-Type': 'appliation/json',
 
 api_url_base = app.config["API_URL_BASE"]
 full_api = app.config["API_FULL_URL"]
+api_by_id = app.config["API_BY_ID"]
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.form:
         print(request.form)
-    return render_template('home.html')
+    return render_template('home.html', api_by_id=api_by_id, favicon='https://www.themoviedb.org/assets/2/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg')
 
 
 if __name__ == "__main__":
@@ -42,10 +43,15 @@ get_movies = get_movies()
 if get_movies is not None:
      print("here's your info: ")
      for k, v in get_movies['results'][0].items():
-         if k == 'title':
-             print(v)
+        if k == 'title':
+            print(f'The title of the movie is: "{v}".')
+        if k == 'release_date':
+            print(f'The release date for the movie is: "{v}".')
+#   TO DO: find out what "popularity means"          
+        if k == 'popularity':
+            print(f'This movie is number "{v}" on the list of the current most popular movies.')
 # all the movies and key/values:
-        #  print('{0}:{1}'.format(k, v))
+        # print('{0}:{1}'.format(k, v))
 
 else:
     print('[!] Request failed')   
