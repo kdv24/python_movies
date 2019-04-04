937fe9322181cc30144cd26a7be1e767ec8a8697
Kelly de Vries <kdv@me.com>
2019-04-04 09:21:04 -0700

    tightened up code & cleaned up logic to return a better object for home.html

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 # ./app.py
 
-from flask import Flask, request, render_template, g
+from flask import Flask, render_template
 import json
 import requests
 
 app = Flask(__name__)
+
+# allows grabbing env variables saved in config file
 app.config.from_object('config')
-
-api_token = app.config["API_TOKEN"]
-
 api_url_base = app.config["API_URL_BASE"]
 api_by_popularity = app.config["API_BY_POPULARITY"]
 
@@ -18,36 +17,32 @@ def get_popular_movies():
     response = requests.get(api_by_popularity)
 
     if response.status_code == 200:
-        return json.loads(response.content.decode('utf-8'))
+        movies = json.loads(response.content.decode('utf-8'))
+        movies = movies['results']
+        return movies
+
     else:
         return None
 
 
-get_popular_movies = get_popular_movies()
-
-
 def get_movie_array():
+    popular_movies = get_popular_movies()
     movie_list = []
-    if get_popular_movies is not None:
-        print("here's your info: ")
-        for results in get_popular_movies['results']:
-            for movie in results.items():
-                movie_list.append(movie)
-        print(movie_list)
-        return movie_list
+
+    if popular_movies is not None:
+        for movie in popular_movies:
+            movie_list.append(
+                {'title': movie['title'], "release_date": movie['release_date'], "popularity": movie['popularity']})
 
     else:
         print('[!] Request failed')
-
-
-get_movie_array = get_movie_array()
+    return (movie_list)
 
 
 @app.route("/", methods=["GET"])
 def home():
-    if request.form:
-        print(request.form)
-    return render_template('home.html', get_movie_array=get_movie_array)
+    movie_array = get_movie_array()
+    return render_template('home.html', movie_array=movie_array)
 
 
 if __name__ == "__main__":
